d4acf3fab14dc1afa7f142c735b40b1f7f962e2e
niruka0918 <niruka@naver.com>
2024-01-09 14:27:18 +0900

    5 commit

--- documentchat.py
+++ documentchat.py
@@ -37,9 +37,7 @@ def main():
 
     with st.sidebar:
         uploaded_files =  st.file_uploader("Upload your file",type=['pdf','docx','pptx'],accept_multiple_files=True)
-        """openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")"""
-        openai_api_key = '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'
-        print(openai_api_key)
+        openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
         process = st.button("Process")
     if process:
         if not openai_api_key:
