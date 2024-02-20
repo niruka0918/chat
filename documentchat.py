af60e3f53df25e59c4640607f62c43b6fec18998
niruka0918 <61739369+niruka0918@users.noreply.github.com>
2024-02-20 23:56:50 +0900

    Update documentchat.py

--- documentchat.py
+++ documentchat.py
@@ -27,7 +27,7 @@ def main():
     page_icon=":books:")
 
     st.title("_KICJ :red[Q/A Document Chat]_ :books:")
-    openai_api_key = st.secrets["openai_api_key"]
+    os.environ["openai_api_key"] = st.secrets["openai_api_key"]
     if "conversation" not in st.session_state:
         st.session_state.conversation = None
 
@@ -39,7 +39,7 @@ def main():
 
     with st.sidebar:
         uploaded_files = st.file_uploader("Upload your file",type=['pdf','docx'],accept_multiple_files=True)
-        
+        openai_api_key = st.secrets["openai_api_key"]
         process = st.button("Process")
     if process:
         if not openai_api_key:
@@ -82,9 +82,9 @@ def main():
 
                 st.markdown(response)
                 with st.expander("참조된 문서"):
-                    st.markdown(source_documents[0].metadata['source'],source_documents[0].metadata['page'], help = source_documents[0].page_content)
-                    st.markdown(source_documents[1].metadata['source'],source_documents[1].metadata['page'], help = source_documents[1].page_content)
-                    st.markdown(source_documents[2].metadata['source'],source_documents[2].metadata['page'], help = source_documents[2].page_content)
+                    st.markdown(source_documents[0].metadata['source'], help = source_documents[0].page_content)
+                    st.markdown(source_documents[1].metadata['source'], help = source_documents[1].page_content)
+                    st.markdown(source_documents[2].metadata['source'], help = source_documents[2].page_content)
                     
 
 
